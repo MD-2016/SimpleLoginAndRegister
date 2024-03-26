09139d71f72f6452fae7dde70b85b69a0431105e
MD <25043596+MD-2016@users.noreply.github.com>
2024-03-26 16:23:19 -0400

    updated user model

--- src/model/user.php
+++ src/model/user.php
@@ -1,13 +1,49 @@
 <?php
 
- include("../config/db.php");
+ include "../config/db.php";
  
  class User {
      public $conn;
 
-    public function addUser($id, $email, $pass, $joined){}
-    public function removeUser($id){}
-    public function selectUser($id){}
-    public function updateUser($id, $email, $pass){}
+    public function addUser($email, $pass){
+        $conn = new Database;
+        if($conn->connect() == null) {
+            echo "connection has died";
+        }
+        $date = date('y-m-d H-m-sa');
+        $sql = "INSERT INTO `Users`(`email`, `password`, `is_admin`,`joined`)VALUES(?,?,0,?)";
+        $stmt = $conn->connect()->prepare($sql);
+        return $stmt = $stmt->execute([$email, $pass, $date]);
+    }
+    public function removeUser($id){
+        $conn = new Database;
+        if($conn->connect() == null) {
+            echo "connection has died";
+        }
+        $sql = "DELETE FROM `Users` WHERE `user_id`=?";
+        $stmt = $conn->connect()->prepare($sql);
+        return $stmt->execute([$id]); 
+    }
+    public function selectAllUser($id){
+        $conn = new Database;
+        if($conn->connect() == null) {
+            echo "connection has died";
+        }
+        $sql = "SELECT `email`,`joined` WHERE user_id=?";
+        $stmt = $conn->connect()->prepare($sql);
+        $stmt->execute();
+        $res = $stmt->fetchAll();
+        return $res;
+    }
+    public function updateUser($id, $email, $pass){
+        $conn = new Database;
+        if($conn->connect() == null) {
+            echo "connection has died";
+        }
+        $sql = "UPDATE `email`, `password` WHERE `user_id`=?";
+        $stmt = $conn->connect()->prepare($sql);
+        $res = $stmt->execute([$email,$pass,$id]);
+        return $res;
+    }
 
  }
