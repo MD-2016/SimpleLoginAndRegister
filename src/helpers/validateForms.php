fcb50f0da15f4ce38960ad4b87bd622b0159e4d1
MD <25043596+MD-2016@users.noreply.github.com>
2024-03-28 11:47:53 -0400

    added helper functions to validate form elements

---
+++ src/helpers/validateForms.php
@@ -0,0 +1,39 @@
+<?php
+
+class Validate {
+    
+    public static function validateEmail($input) {
+        $errors = [];
+        if(empty($input)) {
+            $errors[] = 'Email is required';
+        }
+
+        if(!filter_var($input, FILTER_SANITIZE_EMAIL)) {
+            $errors[] = 'Email must be in proper format with name @ domain';
+        }
+
+        return $errors;
+    }
+
+    public static function validatePassword($password) {
+        $errors = [];
+        if(empty($password)) {
+            $errors[] = 'Password cannot be blank';
+        }
+
+        return $errors;
+    }
+
+    public static function validateRepeatPassword($firstpass, $secondpass) {
+        $errors = [];
+        if(empty($firstpass) || empty($secondpass)) {
+            $errors[] = 'Both password fields must be filled out';
+        }
+
+        if(strncmp($firstpass, $secondpass, count($firstpass)) != 0) {
+            $errors[] = 'Passwords do not match';
+        }
+
+        return $errors;
+    }
+}
